b38001321c4cf172f5748055eeedfbe48af34a2c
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-05-29 12:31:34 +0900

    Add max and min refresh in operation

--- src/operations1.c
+++ src/operations1.c
@@ -4,12 +4,20 @@ void	push_b(t_info* info)
 {
 	ope_push(info->a, info->b);
 	add_opelist(info, "pb");
+	info->a->min = get_min_value(info->a);
+	info->a->max = get_max_value(info->a);
+	info->b->min = get_min_value(info->b);
+	info->b->max = get_max_value(info->b);
 }
 
 void	push_a(t_info* info)
 {
 	ope_push(info->b, info->a);
 	add_opelist(info, "pa");
+	info->a->min = get_min_value(info->a);
+	info->a->max = get_max_value(info->a);
+	info->b->min = get_min_value(info->b);
+	info->b->max = get_max_value(info->b);
 }
 
 void	swap_a(t_info* info)

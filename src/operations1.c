c9afa863983fa78eef7fec0709b6a6fd59c32bd3
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-05-29 23:25:56 +0900

    Refactoring

--- src/operations1.c
+++ src/operations1.c
@@ -1,5 +1,5 @@
 #include "push_swap.h"
-#include <stdio.h>
+
 void	push_b(t_info* info)
 {
 	ope_push(info->a, info->b);
@@ -37,43 +37,4 @@ void	swap_ab(t_info* info)
 	ope_swap(info->a);
 	ope_swap(info->b);
 	add_opelist(info, "ss");
-}
-
-void	rotate_a(t_info* info)
-{
-	if (info->a->top == NULL || info->a->top->next == NULL)
-		return ;
-	ope_rotate(info->a);
-	add_opelist(info, "ra");
-}
-
-void	rotate_b(t_info* info)
-{
-	if (info->b->top == NULL || info->b->top->next == NULL)
-		return ;
-	ope_rotate(info->b);
-	add_opelist(info, "rb");
-}
-
-void	rotate_ab(t_info* info)
-{
-	if (info->a->top == NULL || info->a->top->next == NULL)
-		return ;
-	if (info->b->top == NULL || info->b->top->next == NULL)
-		return ;
-	ope_rotate(info->a);
-	ope_rotate(info->b);
-	add_opelist(info, "rr");
-}
-
-void	reverse_a(t_info* info)
-{
-	ope_reverse(info->a);
-	add_opelist(info, "rra");
-}
-
-void	reverse_b(t_info* info)
-{
-	ope_reverse(info->b);
-	add_opelist(info, "rrb");
 }
---
+++ src/operations2.c
@@ -0,0 +1,40 @@
+#include "push_swap.h"
+
+void	rotate_a(t_info* info)
+{
+	if (info->a->top == NULL || info->a->top->next == NULL)
+		return ;
+	ope_rotate(info->a);
+	add_opelist(info, "ra");
+}
+
+void	rotate_b(t_info* info)
+{
+	if (info->b->top == NULL || info->b->top->next == NULL)
+		return ;
+	ope_rotate(info->b);
+	add_opelist(info, "rb");
+}
+
+void	rotate_ab(t_info* info)
+{
+	if (info->a->top == NULL || info->a->top->next == NULL)
+		return ;
+	if (info->b->top == NULL || info->b->top->next == NULL)
+		return ;
+	ope_rotate(info->a);
+	ope_rotate(info->b);
+	add_opelist(info, "rr");
+}
+
+void	reverse_a(t_info* info)
+{
+	ope_reverse(info->a);
+	add_opelist(info, "rra");
+}
+
+void	reverse_b(t_info* info)
+{
+	ope_reverse(info->b);
+	add_opelist(info, "rrb");
+}
